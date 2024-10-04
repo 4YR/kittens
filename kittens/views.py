bc6c7849bb9d8f5c84527e3d11b35d14283ac771
roman <you@example.com>
2024-10-04 15:21:17 +0400

    Fix views

--- kittens/views.py
+++ kittens/views.py
@@ -1,4 +1,3 @@
-# from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
